bb90cfadf66597d5a400e8327f754483ad7b310d
johnvibar38 <johnvibardev@gmail.com>
2025-11-03 01:13:29 +0800

    Fix deployment: remove topbar, add migrations to startCommand

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,13 +24,6 @@ let liveSocket = new LiveSocket("/live", Socket, {
   hooks: Hooks
 })
 
-// Show progress bar on live navigation and form submits
-// Topbar temporarily disabled for deployment
-// import topbar from "topbar"
-// topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-// window.addEventListener("phx:page-loading-start", info => topbar.show())
-// window.addEventListener("phx:page-loading-stop", info => topbar.hide())
-
 // connect if there are any LiveViews on the page
 liveSocket.connect()
 
